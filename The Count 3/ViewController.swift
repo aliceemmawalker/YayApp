a1b27e2c4f6639da920649f04462010bc6b405cd
mswalker <walker.alice.e@gmail.com>
2016-11-21 10:36:29 +0800

    Initial Commit

---
+++ The Count 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Count 3
+//
+//  Created by Alice Walker on 21/11/2016.
+//  Copyright © 2016 BSD Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
