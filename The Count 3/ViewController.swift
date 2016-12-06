aa0fd9cc20029169de9e6bd43e99ede333c339c9
Alice Emma Walker <walker.alice.e@gmail.com>
2016-12-06 12:12:09 +0800

    Added files for YayApp

---
+++ The Count 3/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  YayApp
+//
+//  Created by Alice Walker on 21/11/2016.
+//  Copyright © 2016 BSD Academy. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+ 
+    var audioPlayer = AVAudioPlayer()
+
+    
+    @IBAction func cheerButton(_ sender: AnyObject) {
+ 
+        let path = Bundle.main.path(forResource: "cheer.mp3", ofType:nil)!
+        print("About to play")
+        let url = URL(fileURLWithPath: path)
+        print("audio playing")
+        do{
+            let sound = try AVAudioPlayer(contentsOf: url)
+            audioPlayer = sound
+            sound.play()
+        }catch{
+            print("error")
+        }
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Count 3Tests/The_Count_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Count_3Tests.swift
+//  The Count 3Tests
+//
+//  Created by Alice Walker on 21/11/2016.
+//  Copyright © 2016 BSD Academy. All rights reserved.
+//
+
+import XCTest
+@testable import The_Count_3
+
+class The_Count_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
